3d722d9ea125f40e7d4f1b4bf688ecccb985fcc3
GeekyChunk <ermia1303@gmail.com>
2022-01-21 14:37:58 +0330

    init

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import Router from './router/Router'
-
+import axios from 'axios'
 const App = () => { return <Router /> }
+
+axios.defaults.baseURL = 'http://api.github.com/' // E.G
 
 ReactDOM.render(
   <Provider store={store}>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 export default function About() {
+  useEffect(() => {
+    axios.get("users/GeekyChunk")
+    .then(res => console.log(res))
+  }, []);
+  
+
   return (
       <>
         <h1> About </h1>
